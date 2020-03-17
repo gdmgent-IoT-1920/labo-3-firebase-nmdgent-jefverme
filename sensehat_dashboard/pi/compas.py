a9b9d176101db150fdbbb54cc5aa3cce7e1b90d8
nmdgent-jefverme <jefvermeireown@gmail.com>
2020-03-17 12:54:49 +0100

    updated dashboard visuals

--- sensehat_dashboard/pi/compas.py
+++ sensehat_dashboard/pi/compas.py
@@ -1,10 +1,9 @@
-from sense_hat import SenseHat
+from sense_hat import *
 import firebase_admin
 from firebase_admin import credentials, firestore
 import time
 
 print('running')
-
 
 # firebase
 cred = credentials.Certificate("/home/pi/Desktop/labo-3-firebase-nmdgent-jefverme/sensehat_dashboard/config/labo-iot-firebase-adminsdk-c3rpv-23eb19a6ee.json")
@@ -19,9 +18,5 @@ sense.set_imu_config(False, False, False)
 sense.clear()
 
 while True:
-    data = {
-        u'temperature': sense.get_temperature(),
-        u'humidity': sense.get_humidity(),
-    }
-    db.collection(u'raspberry_collection').document(u'sensor-data').set(data)
-    time.sleep(60)
+    print(sense.get_compass())
+    time.sleep(4)
